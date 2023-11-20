ed12bdbd692308b17429210ad2ea58080fa1ee42
VatsalGoel3 <144617902+VatsalGoel3@users.noreply.github.com>
2023-11-20 10:11:38 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -37,7 +37,8 @@ def main():
         print("Pending upgrades:")
         print(pending_upgrades)
         print("Running: sudo apt upgrade for pending upgrades")
-        run_command(["sudo", "apt", "update", "-y"])
+        run_command(["sudo", "apt", "upgrade", "-y"])
+        run_command(["sudo", "apt", "update", "dkms"])
     else:
         print("No pending upgrades.")
 
