9b89fdd6ec70b7d9715cd31e18a760a1363ad270
VatsalGoel3 <144617902+VatsalGoel3@users.noreply.github.com>
2023-11-26 20:08:18 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,6 @@
+Certainly! I've added a new function called `update_upgrade_install_dkms` that performs the specified steps for updating, upgrading, and installing `dkms`. This function will be called as the first step for Ubuntu version 22.04. Here's the modified script:
+
+```python
 #!/usr/bin/env python3
 import subprocess
 import fileinput
@@ -8,6 +11,17 @@ def run_command(command):
         return 0, result.stdout
     except subprocess.CalledProcessError as e:
         return e.returncode, e.stderr
+
+def update_upgrade_install_dkms():
+    # Update, upgrade, and install dkms for Ubuntu 22.04
+    print("Running: sudo apt update")
+    run_command(["sudo", "apt", "update"])
+
+    print("Running: sudo apt upgrade")
+    run_command(["sudo", "apt", "upgrade", "-y"])
+
+    print("Running: sudo apt install dkms -y --allow-change-held-packages")
+    run_command(["sudo", "apt", "install", "dkms", "-y", "--allow-change-held-packages"])
 
 def update_release_upgrades_file():
     # Open the file for in-place editing
@@ -21,44 +35,60 @@ def check_and_upgrade_to_latest():
     print("Running: sudo do-release-upgrade -f DistUpgradeViewNonInteractive")
     run_command(["sudo", "do-release-upgrade", "-f", "DistUpgradeViewNonInteractive"])
 
-def main():
-    # Update package information
+def install_python_packages():
+    # Install python3-pip, pandas, and scipy
     print("Running: sudo apt update")
     run_command(["sudo", "apt", "update"])
 
-    # Upgrade installed packages
     print("Running: sudo apt upgrade")
-    return_code, upgrade_output = run_command(["sudo", "apt", "upgrade", "-y"])
+    run_command(["sudo", "apt", "upgrade", "-y"])
 
+    print("Running: sudo apt install python3-pip")
+    run_command(["sudo", "apt", "install", "python3-pip", "-y"])
+
+    print("Running: pip install pandas==1.4.4")
+    run_command(["pip", "install", "pandas==1.4.4"])
+
+    print("Running: pip install scipy==1.6.3")
+    run_command(["pip", "install", "scipy==1.6.3"])
+
+def clone_and_setup_errant():
+    # Clone the Errant repository
+    print("Running: git clone https://github.com/marty90/errant.git")
+    run_command(["git", "clone", "https://github.com/marty90/errant.git"])
+
+    # Set up the path variable
+    print("Setting up path variable")
+    run_command(["export", "PATH=$PATH:/users/VTG003/errant"])
+
+def main():
+    # Check Ubuntu version
+    version_command = ["lsb_release", "-rs"]
+    return_code, version_output = run_command(version_command)
     if return_code != 0:
-        print(f"Error upgrading packages:\n{upgrade_output}")
+        print(f"Error checking Ubuntu version:\n{version_output}")
+        return
+
+    ubuntu_version = version_output.strip()
+    print(f"Detected Ubuntu version: {ubuntu_version}")
+
+    if ubuntu_version == "22.04":
+        # For Ubuntu 22.04, update, upgrade, and install dkms
+        update_upgrade_install_dkms()
+
+        # Continue with other tasks
+        check_and_upgrade_to_latest()
+        install_python_packages()
+        clone_and_setup_errant()
+    elif ubuntu_version == "20.04":
+        # If Ubuntu version is 20.04, install required Python packages and set up Errant
+        install_python_packages()
+        clone_and_setup_errant()
     else:
-        print("Upgrade successful.")
-
-    # Update the release-upgrades file
-    print("Updating /etc/update-manager/release-upgrades file")
-    update_release_upgrades_file()
-
-    # Check for any pending upgrades
-    pending_upgrades = run_command(["apt", "list", "--upgradable"])
-
-    if pending_upgrades:
-        print("Pending upgrades:")
-        print(pending_upgrades)
-        print("Running: sudo apt upgrade for pending upgrades")
-        return_code, upgrade_output = run_command(["sudo", "apt", "upgrade", "-y"])
-        
-        if return_code != 0:
-            print(f"Error upgrading packages:\n{upgrade_output}")
-        else:
-            print("Upgrade successful.")
-
-        # Update dkms after pending upgrades
-        print("Running: sudo apt upgrade dkms")
-        run_command(["sudo", "apt", "upgrade", "dkms", "-y", "--allow-change-held-packages"])
-
-    # Check for a new release and upgrade
-    check_and_upgrade_to_latest()
+        print("Unsupported Ubuntu version. Please upgrade to 20.04 or 22.04.")
 
 if __name__ == "__main__":
     main()
+```
+
+This script will now first update, upgrade, and install `dkms` for Ubuntu 22.04 before proceeding with the other tasks.
