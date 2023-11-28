46f04e15ec8c0820fdc4db0099d6f5b621da43e7
VatsalGoel3 <144617902+VatsalGoel3@users.noreply.github.com>
2023-11-28 09:30:22 -0700

    Update mptcp.sh

--- mptcp.sh
+++ mptcp.sh
@@ -1,40 +1,65 @@
-#For 1
-ip mptcp endpoint flush
-ip mptcp limits set subflow 2 add_addr_accepted 2
+###############################################################
+# Topology
+#  _______   5mbit, 5ms	     _______   10mbit, 10ms   _______
+# |       |-----------------|  r1   |----------------|       |
+# |  h1   |                 |_______|                |   h2  |
+# |       |                  _______                 |       |
+# |       |                 |  r2   |                |       |
+# |_______|-----------------|_______|----------------|_______|
+#	          10mbit, 10ms           50mbit, 10ms       
+##############################################################
 
-ip mptcp endpoint add 10.10.3.1 dev enp6s0f3 id 1 fullmesh
+#!/bin/sh
 
-ip rule add from 10.10.1.1 table 1
-ip rule add from 10.10.3.1 table 2
+sysctl -w net.ipv4.conf.all.rp_filter=0
 
-ip route add 10.10.1.0/24 dev enp4s0f1 scope link table 1
-ip route add default via 10.10.1.2 dev enp4s0f1 table 1
-
-ip route add 10.10.3.0/24 dev enp6s0f3 scope link table 2
-ip route add default via 10.10.3.2 dev enp6s0f3 table 2
-
-ip route add default scope global nexthop via 10.10.1.2 dev enp4s0f1
-
-#For 2
-ip mptcp endpoint flush
-ip mptcp limits set subflow 2 add_addr_accepted 2
-
-ip mptcp endpoint add 10.10.4.2 dev enp4s0f1 id 1 signal
-
-ip rule add from 10.10.2.2 table 3
-ip rule add from 10.10.4.2 table 4
-
-ip route add 10.10.2.0/24 dev enp6s0f3 scope link table 3
-ip route add default via 10.10.2.1 dev enp6s0f3 table 3
-
-ip route add 10.10.4.0/24 dev enp4s0f1 scope link table 4
-ip route add default via 10.10.4.1 dev enp4s0f1 table 4
-
-ip route add default scope global nexthop via 10.10.4.1 dev enp4s0f1
-
-
-#Extra
 sysctl -w net.mptcp.enabled=1
-sysctl -w net.mptcp.checksum_enabled=1
-sysctl -w net.mptcp.pm_type=1
+
 sysctl -w net.mptcp.allow_join_initial_addr_port=1
+
+# Define subflows for MPTCP for node 1
+ip mptcp endpoint flush
+ip mptcp limits set subflow 2 add_addr_accepted 2
+
+# Define subflows for MPTCP for node 4
+ip mptcp endpoint flush
+ip mptcp limits set subflow 2 add_addr_accepted 2
+
+# Path Management 'in-kernel' using ip mptcp for node 1
+ip mptcp endpoint add 192.168.0.1 dev eth2a id 1 fullmesh
+
+# Path Management 'in-kernel' using ip mptcp for node 4
+ip mptcp endpoint add 192.168.1.2 dev eth4b id 1 signal
+
+# Enable IP forwarding for node 2 and 3
+sysctl -w net.ipv4.ip_forward=1
+sysctl -w net.ipv4.ip_forward=1
+
+# Create two routing tables for two interace in node 1
+ip rule add from 10.0.0.1 table 1
+ip rule add from 192.168.0.1 table 2
+
+# Configure the two routing tables
+ip route add 10.0.0.0/24 dev eth1a scope link table 1
+ip route add default via 10.0.0.2 dev eth1a table 1
+
+ip route add 192.168.0.0/24 dev eth2a scope link table 2
+ip route add default via 192.168.0.2 dev eth2a table 2
+
+# Global Default route
+ip route add default scope global nexthop via 10.0.0.2 dev eth1a
+
+
+# Create two routing tables for two interace for node 4
+ip rule add from 10.0.1.2 table 3
+ip rule add from 192.168.1.2 table 4
+
+# Configure the two routing tables
+ip route add 10.0.1.0/24 dev eth3b scope link table 3
+ip route add default via  10.0.1.1 dev eth3b table 3   
+
+ip route add 192.168.1.0/24 dev eth4b scope link table 4
+ip route add default via 192.168.1.1 dev eth4b table 4
+
+# Global Default route
+ip route add default scope global nexthop via 192.168.1.1 dev eth4b
