9e42f9142d21a2227fd9db4eb0068aa3548b6e50
VatsalGoel3 <144617902+VatsalGoel3@users.noreply.github.com>
2023-11-27 20:58:44 -0700

    Update profile.py

--- profile.py
+++ profile.py
@@ -38,8 +38,7 @@ link3 = request.Link(members=[node1, node3])
 link4 = request.Link(members=[node3, node4])
 
 # Add the service to run the setup script on each node
-node1.addService(rspec.Install(url="https://github.com/VatsalGoel3/Multipath-Testbed/raw/main/setup.sh", path="/"))
-node1.addService(rspec.Execute(shell="bash", command="/setup.sh"))
+node1.addService(rspec.Execute(shell="bash", command="/local/repository/setup.sh/"))
 node2.addService(rspec.Execute(shell="bash", command="/local/repository/setup.sh/"))
 node3.addService(rspec.Execute(shell="bash", command="/local/repository/setup.sh/"))
 node4.addService(rspec.Execute(shell="bash", command="/local/repository/setup.sh/"))
