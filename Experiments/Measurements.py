57d19635a8c34eeb043af0ec5e84c98ebef5f8d4
VatsalGoel3 <144617902+VatsalGoel3@users.noreply.github.com>
2023-11-30 11:18:31 -0700

    Update Measurements.py

--- Experiments/Measurements.py
+++ Experiments/Measurements.py
@@ -1,12 +1,32 @@
 import subprocess
 import csv
 import time
-import re
 
 def run_command(command):
     process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
     output, error = process.communicate()
     return output.decode(), error.decode(), process.returncode
+
+def extract_bitrate(iperf_output):
+    lines = iperf_output.split('\n')
+    for line in lines:
+        if "receiver" in line:
+            parts = line.split()
+            if len(parts) >= 7:
+                bitrate = parts[6]
+                return float(bitrate) if bitrate.replace('.', '').isdigit() else None
+    return None
+
+def extract_ping_rtt(ping_output):
+    try:
+        rtt_line = ping_output.split('\n')[-2]
+        rtt_parts = rtt_line.split('=')[-1].strip().split('/')
+        rtt_min, rtt_avg, rtt_max, rtt_mdev = map(lambda x: float(x.split()[0]), rtt_parts)
+        return rtt_min, rtt_avg, rtt_max, rtt_mdev
+    except Exception as e:
+        print(f"Error extracting ping RTT: {e}")
+        return None, None, None, None
+
 
 def main():
     try:
@@ -19,7 +39,7 @@ def main():
 
         for iteration in range(num_iterations):
             # Run iperf3 command
-            iperf_command = f"sudo mptcpize run ip netns exec h1 iperf3 -c 10.0.1.2 -t 10"
+            iperf_command = "sudo mptcpize run ip netns exec h1 iperf3 -c 10.0.1.2 -t 10"
             iperf_output, iperf_error, iperf_returncode = run_command(iperf_command)
 
             print(f"\niperf3 iteration {iteration + 1} output:")
@@ -27,11 +47,13 @@ def main():
             print("iperf3 error:")
             print(iperf_error)
 
-            # Extract average Bitrate from iperf3 output using regular expressions
-            bitrate_match = re.search(r'(\d+\.\d+)\s+Mbits/sec\s+receiver', iperf_output)
-            average_bitrate = float(bitrate_match.group(1)) if bitrate_match else None
+            # Extract average Bitrate from iperf3 output
+            average_bitrate = extract_bitrate(iperf_output)
 
-            print(f"Average Bitrate: {average_bitrate} Mbits/sec")
+            if average_bitrate is not None:
+                print(f"Average Bitrate: {average_bitrate} Mbits/sec")
+            else:
+                print("Error: Unable to extract Average Bitrate.")
 
             # Sleep for 2 seconds after iperf3
             time.sleep(2)
@@ -46,13 +68,12 @@ def main():
             print(ping_error)
 
             # Extract ping statistics (skip the first 2 lines)
-            rtt_match = re.search(r'(\d+\.\d+)/(\d+\.\d+)/(\d+\.\d+)/(\d+\.\d+)\s+ms', ping_output)
-            if rtt_match:
-                rtt_min, rtt_avg, rtt_max, rtt_mdev = map(float, rtt_match.groups())
-            else:
-                rtt_min = rtt_avg = rtt_max = rtt_mdev = None
+            rtt_min, rtt_avg, rtt_max, rtt_mdev = extract_ping_rtt(ping_output)
 
-            print(f"RTT Min: {rtt_min} ms, RTT Avg: {rtt_avg} ms, RTT Max: {rtt_max} ms, RTT Mdev: {rtt_mdev} ms")
+            if rtt_min is not None:
+                print(f"RTT Min: {rtt_min} ms, RTT Avg: {rtt_avg} ms, RTT Max: {rtt_max} ms, RTT Mdev: {rtt_mdev} ms")
+            else:
+                print("Error: Unable to extract Ping RTT.")
 
             # Log information in CSV file
             csv_file = "network_test_results.csv"
