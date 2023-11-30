b94c9eb5a3645e7f186ec1c41728e97b6126b8bf
VatsalGoel3 <144617902+VatsalGoel3@users.noreply.github.com>
2023-11-30 12:04:09 -0700

    Update script.sh

--- Experiments/script.sh
+++ Experiments/script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Default dataset files
-dataset_file1="4G-Telenor.csv"
-dataset_file2="Star.csv"
+dataset_file2="4G-Telenor.csv"
+dataset_file1="Star.csv"
 
 # Fixed namespaces and interfaces
 namespace1="r1"
@@ -11,10 +11,14 @@ interface1="eth3a"
 namespace2="r2"
 interface2="eth4a"
 
+# Set the sleep duration and default iteration number
+sleep_duration=0
+iteration_number=0
+
 while getopts "i:t:" opt; do
     case $opt in
         i)
-            num_iterations="$OPTARG"
+            iteration_number="$OPTARG"
             ;;
         t)
             sleep_duration="$OPTARG"
@@ -30,8 +34,8 @@ while getopts "i:t:" opt; do
     esac
 done
 
-if [[ -z "$num_iterations" || -z "$sleep_duration" ]]; then
-    echo "Usage: $0 -i <num_iterations> -t <sleep_duration>"
+if [[ -z "$iteration_number" || -z "$sleep_duration" ]]; then
+    echo "Usage: $0 -i <iteration_number> -t <sleep_duration>"
     exit 1
 fi
 
@@ -67,30 +71,37 @@ main() {
     dataset1=($(tail -n +2 "$dataset_file1"))
     dataset2=($(tail -n +2 "$dataset_file2"))
 
-    for ((iteration = 0; iteration < num_iterations; iteration++)); do
-        download1=$(echo "${dataset1[iteration % ${#dataset1[@]}]}" | cut -d',' -f1)
-        upload1=$(echo "${dataset1[iteration % ${#dataset1[@]}]}" | cut -d',' -f2)
-        rtt1=$(echo "${dataset1[iteration % ${#dataset1[@]}]}" | cut -d',' -f3)
+    # Ensure the iteration number is within bounds
+    if [ "$iteration_number" -lt 1 ] || [ "$iteration_number" -gt "${#dataset1[@]}" ]; then
+        echo "Invalid iteration number. It should be between 1 and ${#dataset1[@]}."
+        exit 1
+    fi
 
-        # Clear existing network configuration for namespace1 and interface1
-        clear_network_configuration "$namespace1" "$interface1"
+    # Adjust for array indexing (start from 0)
+    ((iteration_number--))
 
-        # Configure the network interface for namespace1 and interface1
-        configure_network_interface "$namespace1" "$interface1" "$download1" "$upload1" "$rtt1"
+    download1=$(echo "${dataset1[iteration_number]}" | cut -d',' -f1)
+    upload1=$(echo "${dataset1[iteration_number]}" | cut -d',' -f2)
+    rtt1=$(echo "${dataset1[iteration_number]}" | cut -d',' -f3)
 
-        download2=$(echo "${dataset2[iteration % ${#dataset2[@]}]}" | cut -d',' -f1)
-        upload2=$(echo "${dataset2[iteration % ${#dataset2[@]}]}" | cut -d',' -f2)
-        rtt2=$(echo "${dataset2[iteration % ${#dataset2[@]}]}" | cut -d',' -f3)
+    # Clear existing network configuration for namespace1 and interface1
+    clear_network_configuration "$namespace1" "$interface1"
 
-        # Clear existing network configuration for namespace2 and interface2
-        clear_network_configuration "$namespace2" "$interface2"
+    # Configure the network interface for namespace1 and interface1
+    configure_network_interface "$namespace1" "$interface1" "$download1" "$upload1" "$rtt1"
 
-        # Configure the network interface for namespace2 and interface2
-        configure_network_interface "$namespace2" "$interface2" "$download2" "$upload2" "$rtt2"
+    download2=$(echo "${dataset2[iteration_number]}" | cut -d',' -f1)
+    upload2=$(echo "${dataset2[iteration_number]}" | cut -d',' -f2)
+    rtt2=$(echo "${dataset2[iteration_number]}" | cut -d',' -f3)
 
-        # Sleep for the specified duration between iterations
-        sleep "$sleep_duration"
-    done
+    # Clear existing network configuration for namespace2 and interface2
+    clear_network_configuration "$namespace2" "$interface2"
+
+    # Configure the network interface for namespace2 and interface2
+    configure_network_interface "$namespace2" "$interface2" "$download2" "$upload2" "$rtt2"
+
+    # Sleep for the specified duration between iterations
+    sleep "$sleep_duration"
 }
 
 # Execute the main function
